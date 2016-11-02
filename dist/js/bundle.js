3fc4b2942a18c350c21e36d0bbb4a31f02fed9db
Antonranestam <anton@ranestam.se>
2016-11-02 18:00:02 +0100

    Start page styling/markup/js

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,6 +1,68 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+
+  $('#start').parallax({
+    calibrateX: true,
+    calibrateY: true,
+    invertX: false,
+    invertY: false,
+    limitX: 10,
+    limitY: 10
+  });
+
+  $('.mute-btn').click(function () {
+    toggleMute();
+
+    $(this).toggleClass('active');
+  });
+});
+
+// Toggle mute for music
+function toggleMute() {
+  var audio = document.getElementById('song');
+
+  if (audio.muted) {
+    audio.muted = false;
+  } else {
+    audio.muted = true;
+  }
+}
+
+// Audio visualizer bar music
+window.onload = function () {
+  var audio = document.getElementById('song');
+  var ctx = new AudioContext();
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+
+  audio.volume = 0.3;
+  audioSrc.connect(analyser);
+  audioSrc.connect(ctx.destination);
+
+  // get data
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+  // Render shake for backgrounds
+  function animateBars() {
+    requestAnimationFrame(animateBars);
+
+    // update data in frequencyData
+    analyser.getByteFrequencyData(frequencyData);
+
+    // Animate audio bars
+    var i;
+
+    for (i = 0; i < 32; i++) {
+      $('.visualizer__wrapper span:nth-child' + '(' + i + ')').css({
+        'height': +frequencyData[i] + 'px',
+        'margin-top': -frequencyData[i] / 3 + 'px'
+      });
+    }
+  }
+
+  animateBars();
+};
 
 },{}]},{},[1]);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
 $(document).ready(function () {
 
+  $('#start').parallax({
+    calibrateX: true,
+    calibrateY: true,
+    invertX: false,
+    invertY: false,
+    limitX: 10,
+    limitY: 10
+  });
+
+  $('.mute-btn').click(function () {
+    toggleMute();
+
+    $(this).toggleClass('active');
+  });
+
 });
+
+// Toggle mute for music
+function toggleMute() {
+  var audio = document.getElementById('song');
+
+  if (audio.muted) {
+    audio.muted = false;
+  } else {
+    audio.muted = true;
+  }
+}
+
+// Audio visualizer bar music
+window.onload = function () {
+  var audio = document.getElementById('song');
+  var ctx = new AudioContext();
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+
+  audio.volume = 0.3;
+  audioSrc.connect(analyser);
+  audioSrc.connect(ctx.destination);
+
+  // get data
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+  // Render shake for backgrounds
+  function animateBars() {
+    requestAnimationFrame(animateBars);
+
+    // update data in frequencyData
+    analyser.getByteFrequencyData(frequencyData);
+
+    // Animate audio bars
+    var i;
+
+    for (i = 0; i < 32; i++) {
+      $('.visualizer__wrapper span:nth-child' + '(' + i + ')').css({
+        'height' : + frequencyData[i] + 'px',
+        'margin-top' : -frequencyData[i] / 3 + 'px'
+      });
+    }
+  }
+
+  animateBars();
+};
