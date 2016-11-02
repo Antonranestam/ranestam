245350f2eac9b134378728213c5042c40b5a3085
Antonranestam <anton@ranestam.se>
2016-11-03 00:29:46 +0100

    Polish of start and initial animation for cases

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -17,8 +17,48 @@ $(document).ready(function () {
 
     $(this).toggleClass('active');
   });
+
+  $('.big-btn').click(function () {
+    animStart();
+  });
 });
 
+// $(window).scroll(function() {
+//   console.log('hej');
+// });
+
+// Start animation for top video and case intro
+
+function animStart() {
+  var tl = new TimelineMax();
+
+  TweenMax.to($('.start-section'), 2, { y: -100 + '%', scale: .4, force3D: true, ease: Power1.easeInOut });
+  TweenMax.to($('.cases'), 1, { y: 0 + '%', force3D: true, ease: Power1.easeInOut, onComplete: colorChange });
+
+  // Fade in menu btn with new color
+  TweenMax.to($('.menu-btn'), .3, { opacity: 0, ease: Power1.easeInOut });
+  TweenMax.to($('.menu-btn'), 1, { opacity: 1, ease: Power1.easeInOut, delay: 1 });
+
+  // Fade in social media with new color
+  TweenMax.to($('.social-media'), .3, { opacity: 0, ease: Power1.easeInOut });
+  TweenMax.to($('.social-media'), 1, { opacity: 1, ease: Power1.easeInOut, delay: 1 });
+
+  // Animate big button up top
+  TweenMax.to($('.big-btn'), 1, { y: 90, ease: Power1.easeInOut });
+  TweenMax.to($('.big-btn'), 0, { opacity: 0, display: 'none', position: 'absolute', bottom: 'auto', top: 'auto', y: -150, ease: Power1.easeInOut, delay: .6 });
+  TweenMax.to($('.big-btn'), 1, { display: 'block', opacity: 1, y: -50, ease: Power1.easeInOut, delay: 1.3 });
+  TweenMax.to($('.big-btn svg'), .3, { margin: '45px auto 0 auto', animation: 'float-reverse 2s infinite ease-in-out', ease: Power1.easeInOut, delay: 1.3 });
+}
+
+function colorChange() {
+
+  // Add class to body to change colors on SM and Menu
+  $('body').addClass('color-change');
+
+  // Turn music down when on next slide
+  var audio = document.getElementById('song');
+  audio.volume = 0;
+}
 // Toggle mute for music
 function toggleMute() {
   var audio = document.getElementById('song');
