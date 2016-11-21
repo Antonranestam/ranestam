81025e51432470a3242b355b66535a431d87a105
Antonranestam <anton@ranestam.se>
2016-11-21 20:00:05 +0100

    First step in making slider cases adjusted for mousewheel

--- public/js/main.js
+++ public/js/main.js
@@ -223,7 +223,7 @@ function menuAnim() {
   TweenMax.to($('.menu__item'), 0, {x: -350 + '%'});
   TweenMax.to($('.line'), .6, {height: 100 + '%', ease:Circ.easeInOut, delay: .1});
 
-  TweenMax.staggerTo($('.menu__item'), 1, {x: '0' + '%', delay: .3, ease:Power1.easeInOut}, .2);
+  TweenMax.staggerTo($('.menu__item'), 1, {x: '0' + '%', ease:Power1.easeInOut}, .2);
 }
 
 function menuAnimBack() {
@@ -375,6 +375,92 @@ window.onload = function () {
 
   animateBars();
 };
+
+// Globals:
+var deltas = [null, null, null, null, null, null, null, null, null],
+    timer  = null,
+    lock   = 0,
+    seen   = 0;
+
+// Search for an inertial peak (which represents a trackpade mouse wheel gesture):
+function hasPeak() {
+
+    // Decrement the lock.
+
+    if (lock > 0) {
+        lock--;
+        return false;
+    }
+
+    // If the oldest delta is null, there can't be a peak yet; so return.
+
+    if (deltas[0] == null) return false;
+
+    // Otherwise, check for a peak signature where the middle delta (4)
+    // is the highest among all other deltas to the left or right.
+
+    if (
+        deltas[0] <  deltas[4] &&
+        deltas[1] <= deltas[4] &&
+        deltas[2] <= deltas[4] &&
+        deltas[3] <= deltas[4] &&
+        deltas[5] <= deltas[4] &&
+        deltas[6] <= deltas[4] &&
+        deltas[7] <= deltas[4] &&
+        deltas[8] <  deltas[4]
+    ) return true;
+
+    // If no peak is found, return false.
+
+    return false;
+}
+
+// Handle mouse wheel events:
+$('.cases').on('mousewheel DOMMouseScroll', function(e) {
+
+    // Convert the delta into a usable number (pretty standard).
+
+    var delta  = e.type == 'mousewheel' ? e.originalEvent.wheelDelta * -1 : 40 * e.originalEvent.detail;
+
+    // Check for an inertial peak. And if found, lock the peak
+    // checking for 10 more events (decremented in hasPeak on
+    // each new event) to prevent the sample window from registering
+    // true more than once for each peak.
+
+    if (hasPeak()) {
+      lock = 10;
+      seen++;
+    }
+
+    // Otherwise, check for normal mouse wheel events by assuming
+    // past and present deltas would be 120 exactly, and skip nulls.
+
+    else if ((deltas[8] == null || deltas[8] == 120) && Math.abs(delta) == 120)
+
+      // @ To do
+      // Make this more dynamic - this is slide change on scroll
+      var casesClient = $('.cases__client');
+      var casesBg = $('.cases__wrapper__article__text');
+      var casesRead = $('.read-btn');
+      var letterWrapper = $('.letter-wrapper');
+
+      $('.cases g').attr("class", "");
+      $('.cases #og').attr("class", "active");
+
+      casesClient.text(casesIntro.og.title);
+      casesBg.html(casesIntro.og.background);
+      casesRead.attr('href', casesIntro.og.link);
+      $('.letter-wrapper').removeClass('ess-bg');
+      $('.letter-wrapper').removeClass('booking-bg');
+      $('.letter-wrapper').addClass('og-bg');
+
+    // Shift the deltas backward and add the newest (maintaining the sample window).
+
+    deltas.shift();
+    deltas.push(Math.abs(delta));
+
+});
+
 
 // Data for cases intros
 var casesIntro = {
